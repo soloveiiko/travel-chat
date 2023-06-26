7943c44456ef6ec6da30f1af92f90d3009f52897
soloveiiko <soloveymaria51@gmail.com>
2023-06-26 10:15:44 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h2 className='text-3xl font-bold underline text-red-600'>Hello world</h2>
+    </div>
+  );
+}
+
+export default App;
