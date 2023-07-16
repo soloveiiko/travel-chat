741c5a7e5a976624ad880d916d327119ba27621a
soloveiiko <soloveymaria51@gmail.com>
2023-07-16 11:00:06 +0300

    added: error pages(403, 404, 500)

---
+++ src/pages/errorPages/NotFoundPage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFoundPage: React.FC = () => {
+  return (
+    <div>
+      <h1>404 Error</h1>
+      <h1>Page Not Found</h1>
+    </div>
+  )
+}
+
+export default NotFoundPage
---
+++ src/pages/errorPages/ServerErrorPage/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ServerErrorPage: React.FC = () => {
+  return (
+    <div>
+      <h1>500 Error</h1>
+      <h1>Page Not Found</h1>
+    </div>
+  )
+}
+
+export default ServerErrorPage
