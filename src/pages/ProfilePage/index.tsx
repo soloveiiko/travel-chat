3eec604097d2b4baa19e7d7e2d12d7deee3bdc3f
soloveiiko <soloveymaria51@gmail.com>
2023-07-17 14:01:47 +0300

    added: settings for profile state

--- src/pages/ProfilePage/index.tsx
+++ src/pages/ProfilePage/index.tsx
@@ -1,14 +1,9 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-import { RootState } from '../../redux/rootReducer'
 
 const ProfilePage: React.FC = () => {
-  const username = useSelector((state: RootState) => state.profile.username)
-  const auth = useSelector((state: RootState) => state.auth)
   return (
     <div>
       <h1 className="text-center">Profile page</h1>
-      <h2>{username}</h2>
     </div>
   )
 }
